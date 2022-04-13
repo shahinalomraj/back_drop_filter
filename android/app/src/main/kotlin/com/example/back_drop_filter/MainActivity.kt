d206045ace03cbc470142ad47dcbe3bd7101b44e
Shahin Alom <shahinalomraj93@gmail.com>
2022-04-13 17:43:24 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/back_drop_filter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.back_drop_filter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                height: 300,
+                width: 300,
+                color: Colors.orange,
+              ),
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Container(
+                  color: Colors.white.withOpacity(0.3),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
